7a44d0fcf0e0621276559264486b0dbb465a7d52
IGassan <igasan96@gmail.com>
2024-02-05 12:56:24 +0300

    fix length-line in progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,7 +2,7 @@ from random import randint
 
 
 QUESTION = 'What number is missing in the progression?'
-PROGRES_LENGTH = 11
+PROGRESSION_LENGTH = 11
 
 
 # Функция для проверки прогрессии
@@ -10,7 +10,8 @@ def asking_question():
     step = randint(1, 10)
     first = randint(1, 10)
     random_number = randint(1, 10)
-    progression = [i for i in range(first, first + PROGRES_LENGTH * step, step)]
+    progression = [i for i in range(first,
+                                    first + PROGRESSION_LENGTH * step, step)]
     currect_answer = progression[random_number]
     progression[random_number] = '..'
     question = ' '.join(map(str, progression))
